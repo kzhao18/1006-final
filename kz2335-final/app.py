9180f00107b13b5763ff04d7730a246304dfd025
Katrina <kz2335@columbia.edu>
2020-05-15 19:57:34 -0400

    Finished classes page

--- kz2335-final/app.py
+++ kz2335-final/app.py
@@ -22,11 +22,11 @@ def test():
 
 """@app.route("/assignments")
 def assignments():
-    return render_template("assignments.html")
-
-@app.route("/assignments")
-def classes():
     return render_template("assignments.html")"""
+
+@app.route("/classes")
+def classes():
+    return render_template("classes.html")
 
 #start the server
 if __name__ == "__main__":
